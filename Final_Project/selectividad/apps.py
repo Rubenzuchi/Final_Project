ebc2d8c2a3404f27996ee071a8354783cb76290e
Rubenzuchi <140895343+Rubenzuchi@users.noreply.github.com>
2024-04-03 22:53:59 +0200

    Delete Final_Project directory

--- Final_Project/selectividad/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SelectividadConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'selectividad'
--- Final_Project/selectividad/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("notas/", views.nota_corte, name="notas"),
-    path("calcular/", views.calcular, name="calcular")
-]
